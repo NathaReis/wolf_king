60af5f935de57040237bb8a09a02a0c7db2371b5
NathaReis <153759333+NathaReis@users.noreply.github.com>
2024-04-10 16:22:34 -0300

    tt

--- PROJETOS/Financeiro/script.js
+++ PROJETOS/Financeiro/script.js
@@ -3,6 +3,14 @@ const $calcBox = document.querySelector(".calcBox")
 const $calcRes = document.querySelector(".calcRes")
 
 let isOperador = false
+let history = []
+
+const resultado = () => {
+    history.push({calc: $calcBox.innerHTML, res: eval($calcBox.innerHTML)})
+    $calcBox.innerHTML = eval($calcBox.innerHTML)
+    $calcRes.innerHTML = ''
+    console.log({calc: $calcBox.innerHTML, res: eval($calcBox.innerHTML)})
+}
 
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -32,8 +40,54 @@ buttons.forEach(btn => {
         $calcRes.innerHTML = eval($calcBox.innerHTML) == undefined ? '' : eval($calcBox.innerHTML)
 
         if(classes.includes('igual')) {
-            $calcBox.innerHTML = eval($calcBox.innerHTML)
-            $calcRes.innerHTML = ''
+            resultado()
         }
     })
+})
+
+window.addEventListener("keydown", (event) => {
+    const keyCode = event.keyCode 
+    //Numero do teclado
+    if(keyCode >= 48 && keyCode <= 57) {[
+        $calcBox.innerHTML += keyCode - 48
+    ]}
+    //Operador do teclado
+    switch(keyCode) {
+        case 189:
+            $calcBox.innerHTML += $calcBox.innerHTML.slice(-1) != '-' ? '-' : ''
+            break
+        case 109:
+            $calcBox.innerHTML += $calcBox.innerHTML.slice(-1) != '-' ? '-' : ''
+            break
+        case 106:
+            $calcBox.innerHTML += $calcBox.innerHTML.slice(-1) != '*' ? '*' : ''
+            break
+        case 106:
+            $calcBox.innerHTML += $calcBox.innerHTML.slice(-1) != '+' ? '+' : ''
+            break
+        case 111:
+            $calcBox.innerHTML += $calcBox.innerHTML.slice(-1) != '/' ? '/' : ''
+            break
+    }
+
+    //Numero
+    if(keyCode >= 96 && keyCode <= 105) {[
+        $calcBox.innerHTML += keyCode - 96
+    ]}
+
+    //Ponto
+    if(keyCode == 190 || keyCode == 110) {
+        if($calcBox.innerHTML.slice(-1) != '.') {
+            $calcBox.innerHTML += '.'
+        }
+    }
+    //Apagar
+    if(keyCode == 8) {
+        $calcBox.innerHTML = $calcBox.innerHTML.slice(0, -1)
+    }
+    $calcRes.innerHTML = eval($calcBox.innerHTML) == undefined ? '' : eval($calcBox.innerHTML)
+    //Enter 
+    if(keyCode == 13) {
+        resultado()
+    }
 })
